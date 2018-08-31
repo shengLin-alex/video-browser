a0135e5f0164af55ded52a7cbd4fd38c7d8615f4
DESKTOP-STF9L29\Sheng-Wei Lin <a182820998@gmail.com>
2018-08-31 17:38:44 +0800

    [開發] Listening for custom events

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div>
+        <SearchBar @termChange="onTermChange"></SearchBar>
+    </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script type="text/javascript">
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    import SearchBar from './components/SearchBar';
+
+    export default {
+        name: 'App',
+        components: {
+            SearchBar
+        },
+        methods: {
+            onTermChange(searchTerm) {
+                console.log(searchTerm);
+            }
+        }
+    };
+
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <input title="Search" @input="onInput"/>
+    </div>
+</template>
+
+<script type="text/javascript">
+    export default {
+        name: 'SearchBar',
+        methods: {
+            onInput: function (event) {
+
+                // 父組件使用 props 傳遞數據給子組件
+                // 子組件使用 $emit 觸發父組件事件
+                this.$emit('termChange', event.target.value);
+            }
+        }
+    };
+</script>
+
+<style>
+
+</style>
